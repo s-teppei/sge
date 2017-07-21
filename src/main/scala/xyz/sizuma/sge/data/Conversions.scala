df2d67d79b31dadcc91051dcf409113496a44157
sizuma <share@sizuma.xyz>
2017-07-21 22:44:52 +0900

    add implicit mod

--- src/main/scala/xyz/sizuma/sge/data/Conversions.scala
+++ src/main/scala/xyz/sizuma/sge/data/Conversions.scala
@@ -1,13 +1,13 @@
 package xyz.sizuma.sge.data
 
-import xyz.sizuma.sge.data.Primitive.{Bool, Chars, Number, Real}
+import xyz.sizuma.sge.data.Primitive.{Bool, Chars, Number, Real, Sequence}
 
 /**
   * Created by Teppei Shiroyama under MIT License.
   */
 object Conversions {
 
-  val intConversion = new PrimitiveConversion[Int] {
+  implicit val intConversion = new PrimitiveConversion[Int] {
     override def toPrimitive(a: Int): Primitive = Number(a)
     override def fromPrimitive(primitive: Primitive): Option[Int] = primitive match {
       case Number(long) => Some(long.toInt)
@@ -21,7 +21,7 @@ object Conversions {
     }
   }
 
-  val longConversion = new PrimitiveConversion[Long] {
+  implicit val longConversion = new PrimitiveConversion[Long] {
     override def toPrimitive(a: Long): Primitive = Number(a)
     override def fromPrimitive(primitive: Primitive): Option[Long] = primitive match {
       case Number(long) => Some(long)
@@ -35,7 +35,7 @@ object Conversions {
     }
   }
 
-  val doubleConversion = new PrimitiveConversion[Double] {
+  implicit val doubleConversion = new PrimitiveConversion[Double] {
     override def toPrimitive(a: Double): Primitive = Real(a)
     override def fromPrimitive(primitive: Primitive): Option[Double] = primitive match {
       case Number(long) => Some(long.toDouble)
@@ -49,7 +49,7 @@ object Conversions {
     }
   }
 
-  val booleanConversion = new PrimitiveConversion[Boolean] {
+  implicit val booleanConversion = new PrimitiveConversion[Boolean] {
     override def toPrimitive(a: Boolean): Primitive = Bool(a)
     override def fromPrimitive(primitive: Primitive): Option[Boolean] = primitive match {
       case Number(0) => Some(false)
@@ -61,5 +61,12 @@ object Conversions {
       case Chars("false") | Chars("f") => Some(false)
       case _ => None
     }
+  }
+
+  implicit def seqToPrimitive[A](implicit toPrimitiveA: ToPrimitive[A]) : ToPrimitive[Seq[A]] = seq => Sequence(seq.map(toPrimitiveA.toPrimitive))
+
+  implicit def seqFromPrimitive[A](implicit fromPrimitive: FromPrimitive[A]) : FromPrimitive[Seq[A]] = {
+    case Sequence(primitives) => Some( primitives.flatMap(fromPrimitive.fromPrimitive) )
+    case _ => None
   }
 }
