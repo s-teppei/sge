40f14d6d4a16ae923da94902464676c0c85bad04
sizuma <share@sizuma.xyz>
2017-07-22 09:13:00 +0900

    add conversion to PointI

--- src/main/scala/xyz/sizuma/sge/unit/PointI.scala
+++ src/main/scala/xyz/sizuma/sge/unit/PointI.scala
@@ -1,6 +1,26 @@
 package xyz.sizuma.sge.unit
 
+import xyz.sizuma.sge.data._
+import xyz.sizuma.sge.data.Primitive._
+import xyz.sizuma.sge.data.Conversions._
 /**
   * Created by Teppei Shiroyama under MIT License.
   */
 case class PointI(x:Int,y:Int,z:Int = 0) extends Point[Int]
+
+object PointI {
+  implicit val conversion:PrimitiveConversion[PointI] = new PrimitiveConversion[PointI] {
+    override def fromPrimitive(primitive: Primitive): Option[PointI] = {
+      primitive match {
+        case d:Dictionary =>
+          for(x <- d.get[Int]("x");
+              y <- d.get[Int]("y") ) yield d.get[Int]("z") match {
+            case Some(z) => PointI(x,y,z)
+            case None => PointI(x,y)
+          }
+        case _ => None
+      }
+    }
+    override def toPrimitive(a: PointI): Dictionary = Dictionary.empty() + ("x",a.x) + ("y",a.y) + ("z",a.z)
+  }
+}
