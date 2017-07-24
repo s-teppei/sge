a52846a5d55698b12f015f00e2730f7dee4cb213
sizuma <share@sizuma.xyz>
2017-07-25 08:25:41 +0900

    change to HasObservableState

--- src/main/scala/xyz/sizuma/sge/component/Entity.scala
+++ src/main/scala/xyz/sizuma/sge/component/Entity.scala
@@ -1,18 +1,18 @@
 package xyz.sizuma.sge.component
 
-import xyz.sizuma.sge.util.{DefaultImpl, Observable}
+import xyz.sizuma.sge.util.{DefaultImpl, HasObservableState, Observable}
 
 /**
   * Created by Teppei Shiroyama under MIT License.
   */
-abstract class Entity[A] extends Observable[A] with DefaultImpl[A] with Component {
+abstract class Entity[A] extends HasObservableState[A] with DefaultImpl[A] with Component {
   private[this] var _state = initialState
 
   def initialState: A
 
-  def state: A = _state
+  override def state: A = _state
 
-  def state_=(newState: A): Unit = {
+  override def state_=(newState: A): Unit = {
     _state = newState
     notifyObservers()
   }
