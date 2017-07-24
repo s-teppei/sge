7b8bbec8a9d5b87f0a573e4b78dede6446ec4880
sizuma <share@sizuma.xyz>
2017-07-24 20:57:19 +0900

    add HashUpdater

---
+++ src/main/scala/xyz/sizuma/sge/component/HasUpdater.scala
@@ -0,0 +1,16 @@
+package xyz.sizuma.sge.component
+
+/**
+  * Created by Teppei Shiroyama under MIT License.
+  */
+trait HasUpdater[A] {
+  self : HasState[A] =>
+
+  def update(oldState:A):A
+
+  def doUpdate():Unit = {
+    val oldState = state
+    val newState = update(oldState)
+    this.state = newState
+  }
+}
