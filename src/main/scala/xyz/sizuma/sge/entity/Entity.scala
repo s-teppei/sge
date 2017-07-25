205aeadf4cc6ab345f43f5b128d80a0cee68fa4c
sizuma <share@sizuma.xyz>
2017-07-25 13:57:02 +0900

    add HasSelfState to Entity

--- src/main/scala/xyz/sizuma/sge/entity/Entity.scala
+++ src/main/scala/xyz/sizuma/sge/entity/Entity.scala
@@ -1,18 +1,10 @@
 package xyz.sizuma.sge.entity
 
+import xyz.sizuma.sge.entity.attribute.HasSelfState
 import xyz.sizuma.sge.util.{DefaultImpl, HasObservableState, Observable}
 
 /**
   * Created by Teppei Shiroyama under MIT License.
   */
-abstract class Entity[A] extends HasObservableState[A] with DefaultImpl[A] {
-  private[this] var _state = initialState
-
-  protected def initialState: A
-
-  override def state: A = _state
-
-  override def state_=(newState: A): Unit = {
-    _state = newState
-  }
+abstract class Entity[A] extends HasObservableState[A] with HasSelfState[A] with DefaultImpl[A] {
 }
