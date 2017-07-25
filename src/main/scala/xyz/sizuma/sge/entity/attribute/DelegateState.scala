570ce84891762f24151ab23fe8b31812edc0ae47
sizuma <share@sizuma.xyz>
2017-07-25 14:39:40 +0900

    add initial state

--- src/main/scala/xyz/sizuma/sge/entity/attribute/DelegateState.scala
+++ src/main/scala/xyz/sizuma/sge/entity/attribute/DelegateState.scala
@@ -6,6 +6,8 @@ package xyz.sizuma.sge.entity.attribute
 trait DelegateState[A] {
   self : HasUUID with HasState[A] =>
 
+  this.state = initialState
+
   def stateHolder:StateHolder[A]
   override protected def state: A = stateHolder(uuid)
   override protected def state_=(newState: A): Unit = stateHolder(uuid) = newState
