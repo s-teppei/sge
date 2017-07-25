ec8f1cf670f8ef59931e1d7de377c35b908b612e
sizuma <share@sizuma.xyz>
2017-07-25 14:38:32 +0900

    add empty

--- src/main/scala/xyz/sizuma/sge/entity/attribute/StateHolder.scala
+++ src/main/scala/xyz/sizuma/sge/entity/attribute/StateHolder.scala
@@ -9,3 +9,11 @@ trait StateHolder[A] {
   def apply(uuid:UUID):A
   def update(uuid: UUID,newState:A):Unit
 }
+
+object StateHolder {
+  def empty[A]:StateHolder[A] = new StateHolder[A] {
+    private[this] var map = Map.empty[UUID,A]
+    override def apply(uuid: UUID): A = map(uuid)
+    override def update(uuid: UUID, newState: A): Unit = map += uuid -> newState
+  }
+}
