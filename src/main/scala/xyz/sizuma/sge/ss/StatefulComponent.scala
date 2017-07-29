69ce259b82b2579e3b7fc730171a3aa328e91816
sizuma <share@sizuma.xyz>
2017-07-29 10:30:00 +0900

    change class to trait

--- src/main/scala/xyz/sizuma/sge/ss/StatefulComponent.scala
+++ src/main/scala/xyz/sizuma/sge/ss/StatefulComponent.scala
@@ -1,11 +1,13 @@
 package xyz.sizuma.sge.ss
 
-import javax.swing.JPanel
+import javax.swing.{JComponent, JPanel}
 
 /**
   * Created by Teppei Shiroyama under MIT License.
   */
-abstract class StatefulComponent[A] extends JPanel {
+trait StatefulComponent[A] {
+
+  self : JComponent =>
 
   protected def state:A
   protected def state_=(newState:A):Unit
